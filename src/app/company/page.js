08ebe7bc225fc2d742d44f49ee18006c60383499
Crl0sDEV <sandrinocarlosmiguel@gmail.com>
2024-10-03 17:14:52 +0800

    add a people

--- src/app/company/page.js
+++ src/app/company/page.js
@@ -6,6 +6,11 @@ const people = [
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
     // More people...
+    {
+      name: 'Carlos Miguel Sandrino',
+      role: 'Web Developer',
+      imageUrl: 'https://images.app.goo.gl/GZCDxtyozSQUr4Wb8',
+    },
   ]
   
   export default function Company() {
